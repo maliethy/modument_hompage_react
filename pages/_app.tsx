60d4e93f82f7e878e3b28827c0ffec9e763c7c73
maliethy <maliethy@gmail.com>
2021-11-18 14:24:49 +0900

    feat:set config nextjs+react-query+tailwind

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import react, { useState } from 'react';
+import { AppProps, NextWebVitalsMetric } from 'next/app';
+
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import 'tailwindcss/tailwind.css';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  console.log(metric);
+}
+
+export default MyApp;
